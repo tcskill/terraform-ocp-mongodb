2035872bc85ee268f168e91584fd107e8ce3a358
Thomas Skill <tskill@us.ibm.com>
2021-10-13 15:07:44 -0500

    srvcerts

--- main.tf
+++ main.tf
@@ -112,13 +112,10 @@ resource "null_resource" "deploy_operator" {
   }
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
 #  CREATE A CA CERTIFICATE
-# ---------------------------------------------------------------------------------------------------------------------
 
 resource "tls_private_key" "ca" {
   algorithm   = "RSA"
-  //ecdsa_curve = "${var.private_key_ecdsa_curve}"
   rsa_bits    = "2048"
 }
 
@@ -133,7 +130,7 @@ resource "tls_self_signed_cert" "ca" {
     organization = "Example, LLC"
   }
   
-  validity_period_hours = 364 * 24
+  validity_period_hours = 730 * 24
   allowed_uses = [
     "digital_signature",
     "content_commitment",
@@ -157,15 +154,74 @@ resource "tls_self_signed_cert" "ca" {
   ]
   dns_names = [ "*.mas-mongo-ce-svc.${var.mongo_namespace}.svc.cluster.local","127.0.0.1","localhost" ]
 
+}
 
-  # Store the CA public key in a file.
-  provisioner "local-exec" {
+# CREATE A TLS CERTIFICATE SIGNED USING THE CA CERTIFICATE
+
+resource "tls_private_key" "cert" {
+  algorithm   = "RSA"
+  rsa_bits    = "2048"
+
+    provisioner "local-exec" {
     command = "echo '${tls_self_signed_cert.ca.cert_pem}' > '${local.tmp_dir}/ca.pem' && chmod 0600 '${local.tmp_dir}/ca.pem'"
   }
 }
 
+resource "tls_cert_request" "cert" {
+  key_algorithm   = "${tls_private_key.cert.algorithm}"
+  private_key_pem = "${tls_private_key.cert.private_key_pem}"
+
+  dns_names = [ "*.mas-mongo-ce-svc.${var.mongo_namespace}.svc.cluster.local","127.0.0.1","localhost" ]
+
+  subject {
+    common_name  = "*.mas-mongo-ce-svc.${var.mongo_namespace}.svc.cluster.local"
+    organization = "Example, LLC"
+  }
+}
+
+resource "tls_locally_signed_cert" "cert" {
+  cert_request_pem = "${tls_cert_request.cert.cert_request_pem}"
+
+  ca_key_algorithm   = "${tls_private_key.ca.algorithm}"
+  ca_private_key_pem = "${tls_private_key.ca.private_key_pem}"
+  ca_cert_pem        = "${tls_self_signed_cert.ca.cert_pem}"
+  is_ca_certificate = true
+
+  validity_period_hours = 730 * 24
+  allowed_uses = [
+    "digital_signature",
+    "content_commitment",
+    "key_encipherment",
+    "data_encipherment",
+    "key_agreement",
+    "cert_signing",
+    "crl_signing",
+    "encipher_only",
+    "decipher_only",
+    "any_extended",
+    "server_auth",
+    "client_auth",
+    "code_signing",
+    "email_protection",
+    "ipsec_end_system",
+    "ipsec_tunnel",
+    "ipsec_user",
+    "timestamping",
+    "ocsp_signing"
+  ]
+
+  # Store the key in a file.
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.cert.private_key_pem}' > '${local.tmp_dir}/server.key' && chmod 0600 '${local.tmp_dir}/server.key'"
+  }
+  # Store the crt in a file.
+  provisioner "local-exec" {
+    command = "echo '${tls_locally_signed_cert.cert.cert_pem}' > '${local.tmp_dir}/server.crt' && chmod 0600 '${local.tmp_dir}/server.crt'"
+  }
+}
+
 resource "null_resource" "deploy_certs" {
-  depends_on = [tls_self_signed_cert.ca, null_resource.deploy_operator]
+  depends_on = [null_resource.deploy_operator]
   triggers = {
     kubeconfig = var.cluster_config_file
     namespace = var.mongo_namespace
@@ -174,7 +230,7 @@ resource "null_resource" "deploy_certs" {
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/deployCerts.sh ${self.triggers.namespace} ${self.triggers.certpath}"
-
+    
     environment = {
       KUBECONFIG = self.triggers.kubeconfig
     }
--- scripts/deployCerts.sh
+++ scripts/deployCerts.sh
@@ -8,9 +8,14 @@ mkdir -p "${TMP_DIR}"
 NAMESPACE="$1"
 CERTPATH="$2"
 
+
+
 if [[ "$3" == "destroy" ]]; then
     kubectl delete configmap mas-mongo-ce-cert-map -n ${NAMESPACE}
+    kubectl delete secret mas-mongo-ce-cert-secret -n ${NAMESPACE}
 else 
-    echo "adding cert configmap..."
+    echo "adding mongon configmap..."
     kubectl create configmap mas-mongo-ce-cert-map --from-file=ca.crt=${CERTPATH}/ca.pem -n ${NAMESPACE}
+    echo "adding mongo secret..."
+    kubectl secret tls mas-mongo-ce-cert-secret --cert=${CERTPATH}/server.crt --key=${CERTPATH}/server.key -n ${NAMESPACE}
 fi
