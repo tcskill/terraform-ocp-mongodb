43d8e34c88f5550e58f0d824165e0416fbc6cb9f
Thomas Skill <tskill@us.ibm.com>
2021-10-13 17:01:01 -0500

    instdstry

--- scripts/deployInstance.sh
+++ scripts/deployInstance.sh
@@ -6,7 +6,7 @@ CHARTS_DIR=$(cd $(dirname $0)/../charts; pwd -P)
 if [[ "$2" == "destroy" ]]; then
     echo "removing mongo release..."
     # remove the the release
-    kubectl delete -f "${CHARTS_DIR}/mas-mongo-ce.yaml" -n ${NAMESPACE}
+    kubectl delete ReplicaSet mas-mongo-ce -n ${NAMESPACE}
 else
     echo "adding mongo release..."
     # deploy the release
