afe46c1044ffe1f8f3e8736c7363dba069705632
Thomas Skill <tskill@us.ibm.com>
2021-10-13 19:37:43 -0500

    dplyfx

--- scripts/deployInstance.sh
+++ scripts/deployInstance.sh
@@ -3,7 +3,7 @@
 NAMESPACE="$1"
 CHARTS_DIR=$(cd $(dirname $0)/../charts; pwd -P)
 
-if [[ "$2" == "destroy" ]]; then
+#if [[ "$2" == "destroy" ]]; then
     #echo "removing mongo release..."
     # remove the the release
     #kubectl delete ReplicaSet mas-mongo-ce -n ${NAMESPACE}
@@ -11,4 +11,4 @@ if [[ "$2" == "destroy" ]]; then
     echo "adding mongo release..."
     # deploy the release
     kubectl apply -f "${CHARTS_DIR}/mas-mongo-ce.yaml" -n ${NAMESPACE}
-fi
+#fi
